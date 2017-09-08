17063deb8b0585b26f66fd1d1cd4a7aa2e8e6a64
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-09-08 12:23:15 -0700

    finalize styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  $(body).style.backgroundImage = "url('../../under_construction.jpg')"
   $('.projects').hover(function(){
     $(this).css('border', 'outset 25px white');
   }, function(){
