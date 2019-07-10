09ca988bf5022c90453e6b84bc9c44ef3b46a05b
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-07-10 11:36:50 -0700

    add

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.projects').hover(function(){
-    $(this).css('border', 'outset 25px white');
-  }, function(){
-    $(this).css('border', 'solid 1px gold');
-  });
-});
